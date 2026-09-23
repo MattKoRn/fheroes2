4211ed1eefb55e1c06e5e81bdfceb6fe7714322a
MattKoRn <mattkorn2@gmail.com>
2026-09-23 15:43:37 +1000

    Show and honor AI hero hire decisions

--- src/fheroes2/castle/castle_town.cpp
+++ src/fheroes2/castle/castle_town.cpp
@@ -100,6 +100,13 @@ int Castle::DialogBuyHero( const Heroes * hero ) const
     StringReplace( str, "%{race}", Race::String( hero->GetRace() ) );
     StringReplace( str, "%{count}", count );
 
+    if ( fheroes2::isAutoPlayPopupTimeoutEnabled() ) {
+        std::string decisionMessage = _( "AI will choose in 5 seconds: %{decision}" );
+        StringReplace( decisionMessage, "%{decision}", fheroes2::getAutoPlayDialogDecisionText( Dialog::OK | Dialog::CANCEL ) );
+        str.append( "\n\n" );
+        str.append( decisionMessage );
+    }
+
     const fheroes2::Text heroDescriptionText( std::move( str ), fheroes2::FontType::normalWhite() );
 
     Resource::BoxSprite rbs( PaymentConditions::RecruitHero(), fheroes2::boxAreaWidthPx );
@@ -164,7 +171,10 @@ int Castle::DialogBuyHero( const Heroes * hero ) const
         }
 
         if ( autoDismiss && autoDismissDelay.isPassed() ) {
-            return buttonOkay.isEnabled() ? Dialog::OK : Dialog::CANCEL;
+            if ( !buttonOkay.isEnabled() ) {
+                return Dialog::CANCEL;
+            }
+            return fheroes2::getAutoPlayDialogDecisionResult( Dialog::OK | Dialog::CANCEL );
         }
     }
 
